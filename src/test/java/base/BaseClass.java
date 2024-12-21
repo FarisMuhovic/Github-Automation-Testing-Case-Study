078b9496119cf021ba4406f542264fc12c2200c6
Faris Muhovic <faris.fare123456@gmail.com>
2024-12-21 12:19:26 +0100

    project structure & base class driver setup

---
+++ src/test/java/base/BaseClass.java
@@ -0,0 +1,42 @@
+package base;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class BaseClass {
+    protected static ChromeOptions options;
+    protected WebDriver driver;
+
+    @BeforeAll
+    static void setupClass() {
+        WebDriverManager.chromedriver().setup();
+
+        options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        options.addArguments("--disable-notifications");
+        options.addArguments("--start-maximized");
+    }
+
+    @BeforeEach
+    void setupTest() {
+        driver = new ChromeDriver(options);
+    }
+
+    @AfterEach
+    void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    @AfterAll
+    static void tearDownClass() {
+        System.out.println("All tests have been executed.");
+    }
+}
